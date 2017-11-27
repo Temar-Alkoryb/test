57ad123c683553bcf6fb4e72c79e6988a5915a68
Artem Rybalko <art_93@ukr.net>
2017-11-27 17:06:00 +0200

    Fix errors

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,27 +8,40 @@
   function MainController($interval) {
     var vm = this;
     var timer;
-    
+
     vm.rows = [];
-    
+
     function Row() {
-      this.date = new Date(Date.now()).toString().split('GMT')[0];
-      this.randomNum = Math.floor(Math.random()*4)*3;
-      this.clicks = 0;
+
+      var newRow = {};
+
+      newRow.date = new Date(Date.now()).toString().split('GMT')[0];
+      newRow.randomNum = Math.floor(Math.random() * 4) * 3;
+      newRow.clicks = 0;
+      newRow.visible = false;
+
+      return newRow;
     }
-    
+
     vm.createRow = function() {
-	  vm.rows.push(new Row());
+      vm.rows.push(new Row());
     }
-    
-    vm.addClick = function(id) { }
+
+    vm.addClick = function(id) {
+      vm.rows[id]['clicks']++;
+      vm.rows[id]['visible'] = !vm.rows[id]['visible'];
+    }
 
     function init() {
-	      timer = $interval(vm.createRow, 10000);    
+      timer = $interval(vm.createRow, 5000);
     }
-    
+
+    vm.$onDestroy = function() {
+      $interval.cancel(timer)
+    };
+
     init();
-    
 
   }
+
 })();
